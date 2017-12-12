8fdcf4535535045a26d1a6e0c8b9d8eb823bd8c7
kslohith <kslohith1729@gmail.com>
2017-12-12 13:50:17 +0530

    pre final

--- app/src/main/java/com/slohith/findurway/MainActivity.java
+++ app/src/main/java/com/slohith/findurway/MainActivity.java
@@ -1,40 +1,23 @@
 package com.slohith.findurway;
 
-import android.content.ContentResolver;
-import android.content.ContentValues;
+
 import android.content.Intent;
-import android.database.CharArrayBuffer;
-import android.database.ContentObserver;
-import android.database.Cursor;
-import android.database.DataSetObserver;
-import android.database.sqlite.SQLiteDatabase;
-import android.net.Uri;
-import android.support.annotation.MainThread;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 
 
 
 public class MainActivity extends AppCompatActivity {
-    private DatabaseReference ref;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button = (Button) findViewById(R.id.button);
-        ref = FirebaseDatabase.getInstance().getReference();
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
