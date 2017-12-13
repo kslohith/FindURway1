dd33fbd81e4b7cbaa41c84efd3cdfd5ed57e7a76
kslohith <kslohith1729@gmail.com>
2017-12-14 00:15:32 +0530

    one bus route  final

--- app/src/main/java/com/slohith/findurway/activity273c.java
+++ app/src/main/java/com/slohith/findurway/activity273c.java
@@ -7,17 +7,153 @@ import android.view.View;
 import android.widget.TextView;
 
 public class activity273c extends AppCompatActivity {
-
+    TextView t;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity273c);
-        TextView t = (TextView) findViewById(R.id.place1);
+             t = (TextView) findViewById(R.id.place1);
         t.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i11 = new Intent(activity273c.this,activitytime11.class);
                 startActivity(i11);
+            }
+        });
+             t = (TextView) findViewById(R.id.place2);
+        t.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i12 = new Intent(activity273c.this,activitytime12.class);
+                startActivity(i12);
+            }
+        });
+        t = (TextView) findViewById(R.id.place3);
+        t.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i13 = new Intent(activity273c.this,activitytime13.class);
+                startActivity(i13);
+            }
+        });
+        t = (TextView) findViewById(R.id.place4);
+        t.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i14 = new Intent(activity273c.this,activitytime14.class);
+                startActivity(i14);
+            }
+        });
+        t = (TextView) findViewById(R.id.place5);
+        t.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i15 = new Intent(activity273c.this,activitytime15.class);
+                startActivity(i15);
+            }
+        });
+        t = (TextView) findViewById(R.id.place6);
+        t.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i16 = new Intent(activity273c.this,activitytime16.class);
+                startActivity(i16);
+            }
+        });
+        t = (TextView) findViewById(R.id.place7);
+        t.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i17 = new Intent(activity273c.this,activitytime17.class);
+                startActivity(i17);
+            }
+        });
+        t = (TextView) findViewById(R.id.place8);
+        t.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i18 = new Intent(activity273c.this,activitytime18.class);
+                startActivity(i18);
+            }
+        });
+        t = (TextView) findViewById(R.id.place9);
+        t.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i19 = new Intent(activity273c.this,activitytime19.class);
+                startActivity(i19);
+            }
+        });
+        t = (TextView) findViewById(R.id.place10);
+        t.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i20 = new Intent(activity273c.this,activitytime20.class);
+                startActivity(i20);
+            }
+        });
+        t = (TextView) findViewById(R.id.place11);
+        t.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i21 = new Intent(activity273c.this,activitytime21.class);
+                startActivity(i21);
+            }
+        });
+        t = (TextView) findViewById(R.id.place12);
+        t.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i22 = new Intent(activity273c.this,activitytime22.class);
+                startActivity(i22);
+            }
+        });
+        t = (TextView) findViewById(R.id.place13);
+        t.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i23 = new Intent(activity273c.this,activitytime23.class);
+                startActivity(i23);
+            }
+        });
+        t = (TextView) findViewById(R.id.place14);
+        t.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i24 = new Intent(activity273c.this,activitytime24.class);
+                startActivity(i24);
+            }
+        });
+        t = (TextView) findViewById(R.id.place15);
+        t.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i25 = new Intent(activity273c.this,activitytime25.class);
+                startActivity(i25);
+            }
+        });
+        t = (TextView) findViewById(R.id.place16);
+        t.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i26 = new Intent(activity273c.this,activitytime26.class);
+                startActivity(i26);
+            }
+        });
+        t = (TextView) findViewById(R.id.place17);
+        t.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i27 = new Intent(activity273c.this,activitytime27.class);
+                startActivity(i27);
+            }
+        });
+        t = (TextView) findViewById(R.id.place18);
+        t.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i28 = new Intent(activity273c.this,activitytime28.class);
+                startActivity(i28);
             }
         });
     }
