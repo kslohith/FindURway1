4c7e5db8e12830a8f2ea230ce04fec4dd61bf6e8
kslohith <kslohith1729@gmail.com>
2017-12-13 22:59:17 +0530

    pree final

--- app/src/main/java/com/slohith/findurway/activity273c.java
+++ app/src/main/java/com/slohith/findurway/activity273c.java
@@ -1,7 +1,10 @@
 package com.slohith.findurway;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class activity273c extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class activity273c extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity273c);
+        TextView t = (TextView) findViewById(R.id.place1);
+        t.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i11 = new Intent(activity273c.this,activitytime11.class);
+                startActivity(i11);
+            }
+        });
     }
+
 }
